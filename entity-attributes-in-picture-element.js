0917300b025a952b9642e507b86bcf38c46b60a1
duycop <duycop@gmail.com>
2020-03-13 17:10:52 +0700

    fix error

--- entity-attributes-in-picture-element.js
+++ entity-attributes-in-picture-element.js
@@ -1,4 +1,4 @@
-﻿/*****************************************************************************/
+/*****************************************************************************/
 /*   File: entity-attributes-in-picture-element.js                           */
 /*   Desc: show attrs in picture-element of HASS ui-lovelace                 */
 /* Create: 11 mar 2020                                                       */
@@ -6,40 +6,48 @@
 /*  Email: duycop@gmail.com                                                  */
 /* Github: https://github.com/duycop                                         */
 /*****************************************************************************/
-class EntityAttributesPicture extends HTMLElement {
+class EntityAttributesCard extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({
       mode: 'open'
     });
   }
-  getAttrs() {
-    var value = '';
-    this.config.attrs.forEach(item => {
-      var v = hass.states[this.config.entity].attributes[item.attr];
-      if (v) {
-        if (typeof item.before !== "undefined") value += item.before;
-        if (item.kalip) v = Math.round(v * item.kalip);
-        value += v;
-        if (typeof item.after !== "undefined") value += item.after;
-      } else value += '#';
-    });
-    return value;
-  }
   setConfig(config) {
-    if (!config.entity) {
-      throw new Error('You need to define an entity');
-    }
-    this.config = config;
     const root = this.shadowRoot;
-    root.innerHTML = getAttrs();
+    const card = document.createElement('div');
+    const content = document.createElement('div');
+    content.innerHTML = `
+      <div id='attributes'></div>
+      `;
+    card.appendChild(content);
+    root.appendChild(card);
+    this.config = config;
   }
+
+  _updateContent(element, value) {
+    element.innerHTML = `${value}`;
+  }
+
   set hass(hass) {
     const root = this.shadowRoot;
-    root.innerHTML = getAttrs();
+    Object.keys(hass.states).sort().forEach(key => {
+      if (key == this.config.entity) {
+        var value = '';
+        this.config.attrs.forEach(item => {
+          var v = hass.states[key].attributes[item.attr];
+          if (v) {
+            value += item.before;
+            value += v;
+            value += item.after;
+          } else value += '#'
+        });
+        this._updateContent(root.getElementById('attributes'), value);
+      }
+    });
   }
   getCardSize() {
     return 1;
   }
 }
-customElements.define('entity-attributes-in-picture-element', EntityAttributesPicture);
+customElements.define('entity-attributes-in-picture-element', EntityAttributesCard);
